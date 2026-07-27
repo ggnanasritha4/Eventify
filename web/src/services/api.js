86ff6de6644581625f480e13f7609638d2c2223a
Gnana Sritha Gouravaram <gnanasritha4@gmail.com>
2026-07-27 23:08:47 +0530

    Connect frontend to Railway backend

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://eventify-production-6296.up.railway.app/api",
 });
 
 export default api;
